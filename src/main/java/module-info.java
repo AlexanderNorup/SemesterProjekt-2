cb3e8f03d3638ccb4ba02a6058e2e343a88a934b
Alexander N <alexander.v.norup@gmail.com>
2021-03-18 13:16:55 +0100

    Added Postgres JDBC to the build.gradle

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module semesterprojekt.main {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
+    requires java.sql;
+    requires postgresql;
 
     opens dk.sdu.seb05.semesterprojekt;
 }
