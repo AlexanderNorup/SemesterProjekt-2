0cfd69d5b4d56202fa2f9f3ec90d61c569906ac0
Victor <zenofwolf@live.dk>
2021-04-06 09:13:02 +0200

    First commit to Victors branch

--- src/main/java/dk/sdu/seb05/semesterprojekt/JavaFXTestController.java
+++ src/main/java/dk/sdu/seb05/semesterprojekt/JavaFXTestController.java
@@ -3,7 +3,7 @@ package dk.sdu.seb05.semesterprojekt;
 public class JavaFXTestController {
 
     public void initialize(){
-        System.out.println("initialize(); kører i JavaFXTestController");
+        System.out.println("initialize(); kører i JavaFXTestController fra Victors branch");
     }
 
 }
