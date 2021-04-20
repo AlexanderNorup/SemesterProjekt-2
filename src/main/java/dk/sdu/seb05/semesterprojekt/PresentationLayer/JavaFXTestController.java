88ed7d85a20c0d3b4395d67b62cfc3376f62a6b6
Victor <35064405+VictorABoye@users.noreply.github.com>
2021-04-20 16:28:35 +0200

    Update JavaFXTestController.java to reflect master

--- src/main/java/dk/sdu/seb05/semesterprojekt/PresentationLayer/JavaFXTestController.java
+++ src/main/java/dk/sdu/seb05/semesterprojekt/PresentationLayer/JavaFXTestController.java
@@ -3,7 +3,7 @@ package dk.sdu.seb05.semesterprojekt.PresentationLayer;
 public class JavaFXTestController {
 
     public void initialize(){
-        System.out.println("initialize(); kører i JavaFXTestController fra Victors branch");
+        System.out.println("initialize(); kører i JavaFXTestController");
     }
 
 }
