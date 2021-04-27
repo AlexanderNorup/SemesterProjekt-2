252f5017e04e7d075c00f5a6a2f89c1f058d57dc
Alexander N <alexander.v.norup@gmail.com>
2021-04-27 13:40:41 +0200

    Added 'temp' dialog for auth.json

--- src/main/java/dk/sdu/seb05/semesterprojekt/PresentationLayer/JavaFXTest.java
+++ src/main/java/dk/sdu/seb05/semesterprojekt/PresentationLayer/JavaFXTest.java
@@ -4,8 +4,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Optional;
 
 public class JavaFXTest extends Application {
 
@@ -13,6 +23,11 @@ public class JavaFXTest extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        if(!checkDatabaseAuth()){
+            askForAuth();
+        }
+
         primaryStage.setTitle("Forside");
         Parent fxmlTest = FXMLLoader.load(getClass().getResource("/fxml/frontpage.fxml"));
         Scene s = new Scene(fxmlTest);
@@ -26,4 +41,46 @@ public class JavaFXTest extends Application {
         instance.setPrimaryStage(primaryStage);
         primaryStage.show();
     }
+
+
+
+    private boolean checkDatabaseAuth(){
+        File auth = new File("auth.json");
+        if(auth.exists()){
+            try {
+                JSONObject auth_obj = new JSONObject(Files.readString(Path.of(auth.toURI()), StandardCharsets.UTF_8));
+                return auth_obj.has("password") && !auth_obj.getString("password").isEmpty();
+            } catch (IOException | JSONException e) {
+                System.out.println("Failed to load file or JSON object: " + e.getMessage());
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return false;
+    }
+
+    private void askForAuth(){
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("PostgresSQL Database Kode");
+        dialog.setHeaderText("PostgresSQL Database Kode");
+        dialog.setContentText("Indtast venligst adgangskoden for at tilgå databasen:");
+
+        // Traditional way to get the response value.
+        Optional<String> result = dialog.showAndWait();
+        if (result.isPresent()){
+            try{
+                String auth = result.get();
+                JSONObject json = new JSONObject();
+                json.put("password", auth);
+                File authFile = new File("auth.json");
+                Files.writeString(Path.of(authFile.toURI()), json.toString(2), StandardCharsets.UTF_8);
+            }catch(IOException | JSONException e){
+                System.out.println("Could not save file: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
 }
